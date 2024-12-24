b3e647f9637ec5a9ca7f35368c7867491fad2007
nithiya-manoharan <nithiyamanoharan2004@gmail.com>
2024-12-24 17:27:33 +0530

    Footer to home changed

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ function Footer(){
         <div className="container">
         <footer className="py-3 my-4">
             <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-                <li className="nav-item"><Link to="/" className="nav-link px-2 text-body-secondary">Home</Link></li>
+                <li className="nav-item"><Link to="/home" className="nav-link px-2 text-body-secondary">Home</Link></li>
                 <li className="nav-item"><Link to="/about" className="nav-link px-2 text-body-secondary">About Us</Link></li>
                 <li className="nav-item"><Link to="/all-items" className="nav-link px-2 text-body-secondary">All Items</Link></li>
                 <li className="nav-item"><Link to="/add-new-item" className="nav-link px-2 text-body-secondary">Add Item</Link></li>
