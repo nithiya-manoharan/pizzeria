d7af9692d3b076737d0be4fc6d66a704bb48bd11
nithiya-manoharan <nithiyamanoharan2004@gmail.com>
2025-02-02 11:13:43 +0530

    url handling code

--- src/App.js
+++ src/App.js
@@ -8,26 +8,28 @@ import DeleteItem from "./DeleteItem";
 import ContactUs from "./ContactUs";
 import Footer from "./Footer";
 import UpdateForm from "./UpdateForm";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App(){
   return(
     <>
-    <Menu/>
+    <Router basename="/pizzeria">
+      <Menu/>
 
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/pizzeria" element={<Home/>} />
-      <Route path="/about" element={<About/>}/>
-      <Route path="/all-items" element={<AllItem/>}/>
-      <Route path="/add-new-item" element={<NewItem/>}/>
-      <Route path="/update-item" element={<UpdateItem/>}/>
-      <Route path="/delete-item" element={<DeleteItem/>}/>
-      <Route path="/contact-us" element={<ContactUs/>}/>
-      <Route path="/update-form/:id" element={<UpdateForm/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/pizzeria" element={<Home/>} />
+        <Route path="/about" element={<About/>}/>
+        <Route path="/all-items" element={<AllItem/>}/>
+        <Route path="/add-new-item" element={<NewItem/>}/>
+        <Route path="/update-item" element={<UpdateItem/>}/>
+        <Route path="/delete-item" element={<DeleteItem/>}/>
+        <Route path="/contact-us" element={<ContactUs/>}/>
+        <Route path="/update-form/:id" element={<UpdateForm/>}/>
+      </Routes>
 
-    <Footer/>
+      <Footer/>
+    </Router>
     </>
   )
 }
